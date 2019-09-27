7fe010dbddc88697aa715e209b1756bb4eefb58f
NikolayValev <45887392+NikolayValev@users.noreply.github.com>
2019-09-26 20:11:08 -0400

    Add files via upload

---
+++ 04-rational/build/CMakeFiles/rational.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rational.dir/includes/rational.cpp.o"
+  "librational.pdb"
+  "librational.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rational.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 04-rational/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/d/Uni/DataStructures/cmps231/code/04-rational
+# Build directory: /mnt/d/Uni/DataStructures/cmps231/code/04-rational/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Tests "testing")
